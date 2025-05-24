52bed5ec9437ac8f8ea17753f2f736aaac707cce
Alexey Belgradski <Alexeybelgradski@yandex.ru>
2025-05-24 17:50:45 +0300

    настроен webpack сделана каталожная структура

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 const initialCards = [
     {
       name: "Архыз",
@@ -24,3 +26,34 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function createCard(cardData, deleteCallback) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+    
+    cardDeleteButton.addEventListener('click', () => {
+        deleteCallback(card);
+    });
+
+    return card;
+}
+
+function deleteButton(card) {
+    card.remove();
+}
+
+initialCards.forEach((cardData) => {
+    const cardArray = createCard(cardData, deleteButton);
+    placesList.append(cardArray);
+})
+
+
